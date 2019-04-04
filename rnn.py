0d18e86fa7262d88e349b0c49cebdf2c4efb27b0
hridyansh <hridyansh68@gmail.com>
2019-04-04 10:29:54 +0530

    model creation

--- rnn.py
+++ rnn.py
@@ -24,3 +24,28 @@ X_train, y_train = np.array(X_train),np.array(y_train)
     
 X_train = np.reshape(X_train,(X_train.shape[0],X_train.shape[1],1))
 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+regressor = Sequential()
+
+regressor.add(LSTM(units = 50,return_sequences = True, input_shape = (X_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50,return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50,return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+regressor.compile(optimizer = "adam", loss = "mean_squared_error")
+
+regressor.fit(X_train,y_train,epochs = 100,batch_size=32)
+
